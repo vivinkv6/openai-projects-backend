2d318f626bef9028d5c8d44294fc3919dd4d7e87
vivinkv6 <vivinkv0@gmail.com>
2023-04-26 10:13:50 +0530

    create openai project backend

---
+++ server.js
@@ -0,0 +1,223 @@
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const cors = require("cors");
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const fetch = require("node-fetch");
+
+app.use(express.json());
+app.use(cors());
+
+//AI Chat
+
+app.post("/aichat", async (req, res) => {
+  const { prompt } = req.body;
+
+  try {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: prompt }],
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) =>
+        res.json({
+          role: data.choices[0].message.role,
+          content: data.choices[0].message.content,
+        })
+      );
+  } catch (err) {
+    res.json({ err: err.message });
+  }
+});
+
+//ask questions
+app.post("/ask", async (req, res) => {
+  const { prompt } = req.body;
+
+  try {
+    const response = await fetch("https://api.openai.com/v1/completions", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "text-davinci-003",
+        prompt: prompt,
+        max_tokens: 3800,
+        temperature: 0,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => res.json(data.choices[0].text.trim()));
+  } catch (err) {
+    res.json({ err: err.message });
+  }
+});
+
+//bug fixer
+app.post("/bugfixer", async (req, res) => {
+  const { prompt } = req.body;
+
+  try {
+    const response = await fetch("https://api.openai.com/v1/completions", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "text-davinci-003",
+        prompt: prompt,
+        max_tokens: 3800,
+        temperature: 0,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => res.json(data.choices[0].text.trim()));
+  } catch (err) {
+    res.json({ err: err.message });
+  }
+});
+
+//Time Complexity
+
+app.post("/complexity", async (req, res) => {
+  const { prompt } = req.body;
+
+  try {
+    const response = await fetch("https://api.openai.com/v1/completions", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "text-davinci-003",
+        prompt: prompt,
+        max_tokens: 3800,
+        temperature: 0,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => res.json(data.choices[0].text.trim()));
+  } catch (err) {
+    res.json({ err: err.message });
+  }
+});
+
+//AI Image generator
+app.post("/img", async (req, res) => {
+  const { prompt } = req.body;
+
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+        body: JSON.stringify({
+          prompt: "A cute baby sea otter",
+          n: 1,
+          size: "1024x1024",
+        }),
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => res.json(data.data[0].url));
+  } catch (err) {
+    res.json({ err: err.message });
+  }
+});
+
+//js Bot
+app.post("/bot", async (req, res) => {
+  const { prompt } = req.body;
+
+  try {
+    const response = await fetch("https://api.openai.com/v1/completions", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "text-davinci-003",
+        prompt: prompt,
+        max_tokens: 3800,
+        temperature: 0,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => res.json(data.choices[0].text.trim()));
+  } catch (err) {
+    res.json({ err: err.message });
+  }
+});
+
+//answer the quiz questions
+app.post("/quiz", async (req, res) => {
+  const { prompt } = req.body;
+
+  try {
+    const response = await fetch("https://api.openai.com/v1/completions", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "text-davinci-003",
+        prompt: prompt,
+        max_tokens: 3800,
+        temperature: 0,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => res.json(data.choices[0].text.trim()));
+  } catch (err) {
+    res.json({ err: err.message });
+  }
+});
+
+//generate sql commands
+app.post("/sql", async (req, res) => {
+  const { prompt } = req.body;
+
+  try {
+    const response = await fetch("https://api.openai.com/v1/completions", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "text-davinci-003",
+        prompt: prompt,
+        max_tokens: 3800,
+        temperature: 0,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => res.json(data.choices[0].text.trim()));
+  } catch (err) {
+    res.json({ err: err.message });
+  }
+});
+
+app.listen(process.env.PORT, () => console.log(`Server Run Successfully`));
